b3897b16ccd6e776459a6c986ce5aff3962aa3f0
Vishal <visku199@gmail.com>
2025-10-16 23:32:33 +0530

    setup redux store

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,20 +1,29 @@
 import axios from "axios";
 import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utils/constants";
 
 export const Login = () => {
   const [emailId, setEmailId] = useState("chandan@gmail.com");
   const [password, setPassword] = useState("Chandan@123");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const handleLoginClick = async () => {
     try {
       const res = await axios.post(
-        "http://localhost:3000/login",
+        BASE_URL + "/login",
         {
           emailId,
           password,
         },
         { withCredentials: true }
       );
+      console.log(res.data);
+      dispatch(addUser(res.data));
+      return navigate("/");
     } catch (err) {
       console.error(err);
     }
